48317634c357e8e6b43f27095d8671efd90fa20f
Benjamin Besse <benjamin.besse02@gmail.com>
2017-01-15 13:45:17 +0100

    + initialisation du projet

---
+++ scripts/init.js
@@ -0,0 +1,59 @@
+faker.locale = "en"
+
+const STEPCOUNT = 100; //total 10000 * 100 = 1000000
+
+function isRandomBlank(blankWeight) {
+    return Math.random() * 100 <= blankWeight;
+};
+
+function randomizer(max) {
+  return Math.round(Math.random() * max);
+}
+
+const CATEGORIES = [
+    'Vulgarisation',
+    'Placement produit',
+    'Tuto rapide',
+    'Mode',
+    'Déco'
+];
+
+const WEBSITES = [
+    'JV.com',
+    'Developpez.com',
+    'Mode&Co',
+    'Deco&Co'
+];
+
+for (let i = 0; i < 10000; i++) {
+    db.getCollection("rawData").insertMany(
+        _.times(STEPCOUNT, () => {
+            let doc = {
+                "firstname": faker.name.firstName(),
+                "lastname": faker.name.lastName(),
+                "login": faker.internet.userName(),
+                "password": faker.internet.password(),
+                "lastConnectionDate": faker.date.past(),
+                "website" : WEBSITES[randomizer(WEBSITES.length - 1)],
+                "articles": []
+            };
+
+            let nbArticles = Math.floor(Math.random() * 50);
+
+            for (let j = 0; j < nbArticles; ++j) {
+                doc.articles.push({
+                    "title": faker.lorem.words(),
+                    "content": faker.lorem.paragraphs(),
+                    "date": faker.date.past(),
+                    "nbLike": faker.random.number(),
+                    "category" : CATEGORIES[randomizer(CATEGORIES.length - 1)]
+                });
+            }
+
+
+            return doc;
+        })
+    )
+
+    console.log(`${(i + 1) * STEPCOUNT} docs inserted`);
+}
---
+++ tps/tp1-mapreduce.markdown
@@ -0,0 +1,47 @@
+# TP 1 : MapReduce
+
+Voici le schéma de la collection sur lequel vous allez travailler :
+
+```
+{
+    "firstname": String,
+    "lastname": String,
+    "login": String,
+    "password": String,
+    "lastConnectionDate": Date,
+    "website" : String,
+    "articles": [
+        {
+            "title": String,
+            "content": String,
+            "date": String,
+            "nbLike": String,
+            "category" : String
+        }
+    ]
+}
+```
+
+Il s'agit d'une collection d'utilisateurs avec la liste des articles qu'ils ont postés. Un utilisateur peut avoir entre 0 et 50 articles et la collection contient 15700 enregistrements.
+
+Pour pouvoir travailler deans de bonnes conditions, je vous conseille d'avoir mongoDB 3.2+ d'installé sur vos machines et de passer par un gestionnaire graphique comme mongobooster ou robomongo.
+
+Grâce à mapReduce, réalisez les requêtes suivantes :
+
+**Attention** : Chaque résultat sera à mettre dans une collection différente à chaque fois.
+
+1. 
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+11.
+12.
+13.
+14.
+15.
